4ba683007fa40e1788fc81d05c1ec5ebf4db5d30
wanghy2007 <wanghy2007@gmail.com>
2020-06-30 12:05:37 -0700

    Loading person

--- complete/src/main/java/com/example/accessingdatarest/Person.java
+++ complete/src/main/java/com/example/accessingdatarest/Person.java
@@ -15,6 +15,15 @@ public class Person {
 	private String firstName;
 	private String lastName;
 
+	public Person() {
+
+	}
+
+	public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
