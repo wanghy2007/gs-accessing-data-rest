dbc6ceff7ee1ca6fb0583f0f5a48e1b117092669
wanghy2007 <wanghy2007@gmail.com>
2020-07-02 09:36:51 -0700

    Change from country to countries

--- complete/src/main/java/com/example/accessingdatarest/entity/Country.java
+++ complete/src/main/java/com/example/accessingdatarest/entity/Country.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-@Entity
+@Entity(name = "Countries")
 public class Country {
 
 	private @Id @Column(length = 2) String countryId;
